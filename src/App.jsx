b27ae4dc9fd76fb51cc7d5a464d939c4497c9ab8
Ulgamon <justinilic1@gmail.com>
2023-11-02 09:36:58 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,42 @@
-import { useState } from "react";
-import {
-  useSpring,
-  useTransition,
-  useChain,
-  config,
-  animated,
-  useSpringRef,
-} from "@react-spring/web";
-import data from "./data";
-
+import { useState, useRef } from "react";
+import { useSpring, animated } from "@react-spring/web";
 import "./App.css";
 
 function App() {
-  const [open, setOpen] = useState(false);
+  const cardRef = useRef(null);
 
-  const springApi = useSpringRef();
-  const { size, ...rest } = useSpring({
-    ref: springApi,
-    config: config.stiff,
-    from: {
-      size: "20%",
-      background: "hotpink",
-    },
-    to: {
-      size: open ? "100%" : "20%",
-      background: open ? "white" : "hotpink",
-    },
-  });
+  const [{ xys }, api] = useSpring(() => ({ xys: [0, 0, 1] }));
 
-  const transApi = useSpringRef();
-  const transition = useTransition(open ? data : [], {
-    ref: transApi,
-    trail: 400 / data.length,
-    from: { opacity: 0, scale: 0 },
-    enter: { opacity: 1, scale: 1 },
-    leave: { opacity: 0, scale: 0 },
-  });
+  const handleMouseLeave = () =>
+    api.start({
+      xys: [0, 0, 1],
+    });
 
-  useChain(open ? [springApi, transApi] : [transApi, springApi], [
-    0.1,
-    open ? 0.1 : 0.6,
-  ]);
-
+  const handleMouseMove = (e) => {
+    const rect = cardRef.current.getBoundingClientRect();
+    api.start({
+      xys: calc(e.clientX, e.clientY, rect),
+    });
+  };
   return (
-    <div className="wrapper">
+    <div className="card-main" ref={cardRef}>
       <animated.div
-        style={{ ...rest, width: size, height: size }}
-        className="container"
-        onClick={() => setOpen((open) => !open)}
-      >
-        {transition((style, item) => (
-          <animated.div
-            className="item"
-            style={{ ...style, background: item.css }}
-          />
-        ))}
-      </animated.div>
+        className="card"
+        style={{ transform: xys.to(trans) }}
+        onMouseLeave={handleMouseLeave}
+        onMouseMove={handleMouseMove}
+      />
     </div>
   );
 }
+
+const calc = (x, y, rect) => [
+  -(y - rect.top - rect.height / 2) / 5,
+  (x - rect.left - rect.width / 2) / 5,
+  1.4,
+];
+
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
 export default App;
